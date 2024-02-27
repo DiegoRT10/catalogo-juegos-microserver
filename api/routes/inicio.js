88cbbf4a5a79dc58ed10c28598e64823e6a4411d
DiegoRT10 <alexisdieram@gmail.com>
2024-02-27 17:49:53 -0600

    Se modifico el titulo de inicio de

--- api/routes/inicio.js
+++ api/routes/inicio.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 router.get('/',(reg, res) => {
     console.log('entre a la ruta ');
-    res.send('Microservicio de catalogo de juegos');
+    res.send('Microservicio de catalogo de juego');
 });
 
   
