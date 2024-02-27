53743edf69d7bd29a918adedc8c7b05c6417dc71
DiegoRT10 <alexisdieram@gmail.com>
2024-02-27 16:20:51 -0600

    Se modifico el test

--- api/tests/catalogo.test.js
+++ api/tests/catalogo.test.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 const app = require('../../server');
 const pool = require('../connection/connection');
 
+//
+
 describe('Endpoint /games', () => {
   it('Debería obtener datos de juegos en GET /games', async () => {
     const response = await request(app)
